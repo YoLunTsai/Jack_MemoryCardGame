3c5d75849af62251d6bc571e84fba86405940af9
YoLunTsai <qaz6579@yahoo.com.tw>
2020-07-07 01:40:21 +0800

    遊戲完成判斷函式完成

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 var startButton = document.getElementById("start_button");
-var game_status = false; //遊戲當前狀態，false尚未開始、true已開始。
 var allCards = document.querySelectorAll(".card");
+var allbacks = document.querySelectorAll(".back ,.font");
 
+var game_status = false; //遊戲當前狀態，false尚未開始、true已開始。
 var flippedCard = false;
 var lockCard = false;
 var firstCard, secondCard;
+var count = 0; //配對成功次數，用於判斷遊戲是否已經完成。
 
 function game_start() { //點按鈕開始遊戲
+    document.getElementById("game_text").style.display = "none";//隱藏遊戲完成的文字
     if (game_status == false) {
         //洗牌
         allCards.forEach(Cards => {
@@ -15,9 +18,8 @@ function game_start() { //點按鈕開始遊戲
         });
 
         //翻開全部牌
-        var allbacks = document.querySelectorAll(".back ,.font");
-        allbacks.forEach(e => e.classList.add("after"));
-        setTimeout(function () {
+        allbacks.forEach(e => e.classList.add("after")); //翻牌
+        setTimeout(function () { //等待0.6秒後蓋牌
             allbacks.forEach(e => e.classList.remove("after"))
         }, 600);
 
@@ -29,8 +31,7 @@ function game_start() { //點按鈕開始遊戲
 }
 
 function flipCard() { //開始翻牌
-    if (game_status == true) { //判斷遊戲當前狀態為true時，才可以開始點牌
-
+    if (game_status == true) { //判斷遊戲當前狀態為true時，才可以開始翻牌
         //如果配對失敗，就蓋牌
         if (lockCard) return;
 
@@ -57,17 +58,20 @@ function checkMatch() { //確認是否配對成功
     isMatch ? Match() : unMatch();
 }
 
-function Match() {
+function Match() { //配對成功
+    //移除click事件
     firstCard.removeEventListener("click", flipCard);
     secondCard.removeEventListener("click", flipCard);
 
     resetCard();
+    count++;
+    if (count === 6) gameDone();
 }
 
-function unMatch() {
+function unMatch() { //配對失敗
     lockCard = true;
 
-    setTimeout(() => {
+    setTimeout(() => { //一秒後蓋牌
         firstCard.children[0].classList.remove("after");
         firstCard.children[1].classList.remove("after");
         secondCard.children[0].classList.remove("after");
@@ -81,6 +85,19 @@ function resetCard() {
     [firstCard, secondCard] = [null, null];
 }
 
+function gameDone() { //當count為6時，代表Match執行了6次(也就是12張牌都配對成功)表示遊戲完成
+    setTimeout(function () { //要等待最後一張牌備完全翻過來，才顯示遊戲完成的文字，所以設定0.6秒的延遲
+        document.getElementById("game_text").style.display = "block";
+    }, 600);
+
+    setTimeout(function () { //1秒後再次把所有牌翻回背面，以便重新開始遊戲
+        allbacks.forEach(e => e.classList.remove("after"));
+    }, 1000);
+
+    game_status = false; //將遊戲當前狀態改為false
+    count = 0; //把配對成功次數歸零
+    allCards.forEach(Cards => Cards.addEventListener('click', flipCard)); //再次註冊click事件，以便再次遊玩
+}
 
 startButton.addEventListener('click', game_start);
 allCards.forEach(Cards => Cards.addEventListener('click', flipCard));
